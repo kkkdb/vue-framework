c679995c086a472d017ef12dfe924a1cea8dedb2
CG <GC@CGdeMacBook-Air.local>
2016-07-24 14:31:22 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
-import App from './App'    // 引入根组件
-import routerMap from './router'    // 引入路由器模块
+import App from './App'    // 根组件
+import routerMap from './router'    // 路由器模块
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
@@ -12,4 +12,4 @@ const router = new VueRouter()
 
 routerMap(router)
 
-router.start(App, '#app')
+router.start(App, '#root')
