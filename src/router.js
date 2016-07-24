d81c9d9d0a607b0b828feae6a86e2e736a18516a
CG <GC@CGdeMacBook-Air.local>
2016-07-24 14:38:19 +0800

    组件按需加载

--- src/router.js
+++ src/router.js
@@ -1,43 +1,36 @@
 // 路由器模块
-import index from './view/index'
-import login from './view/login'
-import home from './view/home'
-import productList from './view/product/pdtList'
-import productDetail from './view/product/pdtDetail'
-import inventoryList from './view/inventory/intList'
-import inventoryDetail from './view/inventory/intDetail'
 
 export default function(router) {
     router.map({
         '/index': {
             name: 'index',
-            component: index,
+            component: require('./view/index'),
             subRoutes: {
                 '/home': {
                     name: 'home',
-                    component: home
+                    component: require('./view/home')
                 },
                 '/productList': {
                     name: 'productList',
-                    component: productList
+                    component: require('./view/product/pdtList')
                 },
                 '/productDetail': {
                     name: 'productDetail',
-                    component: productDetail
+                    component: require('./view/product/pdtDetail')
                 },
                 '/inventoryList': {
                     name: 'inventoryList',
-                    component: inventoryList
+                    component: require('./view/inventory/intList')
                 },
                 '/inventoryDetail': {
                     name: 'inventoryDetail',
-                    component: inventoryDetail
+                    component: require('./view/inventory/intDetail')
                 }
             }
         },
         '/login': {
             name: 'login',
-            component: login
+            component: require('./view/login')
         }
     });
 
